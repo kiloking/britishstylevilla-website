8c6140f5b24f423d9e8296f54043c1f712a63bc8
wood.wu <kilokingw@gmail.com>
2022-08-09 19:21:04 +0800

    Update Section7.js

--- src/Section7.js
+++ src/Section7.js
@@ -22,7 +22,7 @@ function Section7() {
   //TEST: https://sheet.best/api/sheets/c045db18-f183-40d4-8e7f-ac7f53cb603f
   const onSubmit = data => {
     console.log(data)
-    axios.post('https://sheet.best/api/sheets/c045db18-f183-40d4-8e7f-ac7f53cb603f/tabs/web02', data)
+    axios.post('https://sheet.best/api/sheets/c045db18-f183-40d4-8e7f-ac7f53cb603f/tabs/web03', data)
     .then(response => {
       console.log(response);
       sendFormStatusModal()
@@ -35,6 +35,13 @@ function Section7() {
     <div className='min-h:100vh bg:white bg:center bg:no-repeat bg:cover   bg:scroll@lg'
     style={{backgroundImage: `url(${process.env.PUBLIC_URL +'/images/section7/section7_bg.png'})`}}
     >
+    {formStatus && <div className={'fixed w:full  z:40 inset:0 transition '} onClick={()=>setFormStatus(false)}>
+        <div className={`w:3/5 mx:auto  r:10 p:10 bg:#000000b4 f:white f:12 top:15% rel transition duration:300 delay:1000   ${formStatus ? " opacity:1 blur(0) " : "opacity:0 blur(0.8) "}`}>
+        表單已送出。我們將盡快電話聯絡您，詢問並安排您方便的賞屋時間。如有任何問題，歡迎直接聯繫我們，謝謝！
+        <div className=' abs top:-10 right:-10 p:5 bg:black r:10 cursor:pointer '><MdClear /></div>
+        </div>
+      
+      </div>}
       <div className='flex flex:col jc:center w:80% mx:auto pt:15% rel z:1 pt:10%@lg w:20%@lg w:50%@sm'>
         <div className='w:full center mb:20' 
           data-aos="fade" data-aos-duration="1500" >
@@ -46,7 +53,7 @@ function Section7() {
             <a href="https://www.facebook.com/profile.php?id=100083270795919" target="_blank" rel="noreferrer" title="link-fb" className='link-fb'>
               <img src={process.env.PUBLIC_URL+'/images/section7/fb.png'} alt="" className='w:full'/> 
             </a>
-            <a href="tel:033811288" target="_blank" rel="noreferrer" title="link-tel" className='link-tel'>
+            <a href="tel:034986766" target="_blank" rel="noreferrer" title="link-tel" className='link-tel'>
               <img src={process.env.PUBLIC_URL+'/images/section7/tel.png'} alt="" className='w:full' /> 
             </a>
             <a href="https://goo.gl/maps/naQagjYP4K4obbzRA" target="_blank" rel="noreferrer" title="link-map" className='link-map'>
@@ -60,18 +67,22 @@ function Section7() {
         <form onSubmit={handleSubmit(onSubmit)} className="rel w:100% mx:auto my:30 w:40%@lg w:40%@sm">
         <img src={process.env.PUBLIC_URL+'/images/section7/section7_line.png'} alt="" className='w:50% abs top:-30 z:-1'/> 
           <div className='w:full flex ai:center my:10 jc:end flex:col@sm ai:start@sm '>
+            <label htmlFor="" className='f:#0667A2 f:bold mr:5 t:13'>姓名/</label>
             <input type="text" className="block  bg:white ml:3 r:5  w:full@sm
                 px:6 py:5 " placeholder="姓名"    {...register("name", { required: true})}/>
           </div>
           <div className='w:full flex ai:center my:10 jc:end flex:col@sm ai:start@sm '>
+            <label htmlFor="" className='f:#0667A2 f:bold mr:5 t:13'>電話/</label>
             <input type="text" className="block  bg:white ml:3 r:5 w:full@sm   
                 px:6 py:5 " placeholder="電話"    {...register("tel", { required: true})}/>
           </div>
           <div className='w:full flex ai:center my:10 jc:end flex:col@sm ai:start@sm '>
+            <label htmlFor="" className='f:#0667A2 f:bold mr:5 t:13'>居住區域/</label>
             <input type="text" className="block  bg:white ml:3 r:5  w:full@sm
                 px:6 py:5 " placeholder="居住區域"    {...register("place", { required: true})}/>
           </div>
           <div className='w:full flex ai:center my:10 jc:end flex:col@sm ai:start@sm '>
+            <label htmlFor="" className='f:#0667A2 f:bold mr:5 t:13'>預約訊息/</label>
             <input type="text" className="block  bg:white ml:3 r:5  w:full@sm
                 px:6 py:5 " placeholder="預約訊息"    {...register("msg")}/>
           </div>
